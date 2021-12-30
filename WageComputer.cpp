b7bc0b8625d9e2a4dec3463071153d1478b0125d
Teo Espero <38012835+teoespero@users.noreply.github.com>
2021-12-29 16:53:29 -0800

    Add files via upload

---
+++ WageComputer.cpp
@@ -0,0 +1,144 @@
+// WageComputer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Teodulfo Espero
+// BS Cloud and Systems Administration
+// BS Software Development
+// Western Governors University
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    //------------------------------------------------------------------------
+    //   define the CONSTANTS that we will be using in this program
+    const double 
+        SOCIALSECURITYTAXRATE = (6.00 / 100.00),
+        STATEINCOMETAXRATE = (5.00 / 100.00),
+        HEALTHCOVERAGECOSTPERDEPENDENT = 35.00,
+        WAGE = 16.78,
+        FEDERALINCOMETAXRATE = (14.00 / 100.00);
+
+
+    //------------------------------------------------------------------------
+    //   define the vars that we will be using in this program
+
+    char
+        again = 'y';
+
+    double
+        hoursworked = 0.00,
+        numberofdependents = 0.00,
+        overtimehours = 0.00,
+        overtimerate = 0.00,
+        overtimepay = 0.00,
+        regularpay = 0.00,
+        socialsecuritytax = 0.00,
+        federalincometax = 0.00,
+        stateincometax = 0.00,
+        uniondues = 10.00,
+        additionalhealthcoveragecost = 0.00,
+        grosspay = 0.00,
+        regularhoursworked = 0.00,
+        netpay = 0.00,
+        deductions = 0.00;
+
+    bool
+        valihoursworked = false,
+        validenumberofdependents = false,
+        endtask = false;
+
+
+
+    //------------------------------------------------------------------------
+    //  start
+
+    do {
+        cout << "-----------------------------------------------------" << endl;
+        cout << "JNC Credit and Financing" << endl << endl;
+        cout << "Payroll program" << endl;
+
+        //  get number of hours worked
+        do {
+            cout << endl << "\t" << "Hours worked: ";
+            cin >> hoursworked;
+            if (hoursworked <= 0.00)
+                cout << endl << "\t" << "*** Hours worked should be > 0." << endl;
+            else
+                valihoursworked = true;
+        } while (!(valihoursworked));
+
+        //  get number of dependents
+        do {
+            cout << "\t" << "Number of dependents: ";
+            cin >> numberofdependents;
+            if (numberofdependents < 0.00)
+                cout << endl << "\t" << "*** Number of dependents should be 0 or more." << endl;
+            else
+                validenumberofdependents = true;
+        } while (!(validenumberofdependents));
+
+        //  does the employee have OT
+        if (hoursworked > 40) {
+            overtimehours = hoursworked - 40;
+            regularhoursworked = hoursworked - overtimehours;
+            overtimerate = WAGE * 1.5;
+            overtimepay = overtimehours * overtimerate;
+        }
+        else
+            regularhoursworked = hoursworked;
+
+        //------------------------------------------------------------------------
+        //  compute the gross pay
+        regularpay = regularhoursworked * WAGE;
+        grosspay = regularpay + overtimepay;
+
+        //------------------------------------------------------------------------
+        //  compute the deductions
+        socialsecuritytax = grosspay * SOCIALSECURITYTAXRATE;
+        federalincometax = grosspay * FEDERALINCOMETAXRATE;
+        stateincometax = grosspay * STATEINCOMETAXRATE;
+        additionalhealthcoveragecost = numberofdependents * HEALTHCOVERAGECOSTPERDEPENDENT;
+
+        deductions = (socialsecuritytax + federalincometax + stateincometax + additionalhealthcoveragecost);
+
+        //------------------------------------------------------------------------
+        //  netpay computations
+        netpay = grosspay - deductions;
+
+
+        //------------------------------------------------------------------------
+        //  print the pay stub
+
+        cout << endl << endl << endl << endl << "-----------------------------------------------------" << endl;
+        cout << "JNC Credit and Financing" << endl << endl;
+        cout << "Pay Stub" << endl;
+        cout << "-----------------------------------------------------" << endl;
+        cout << setprecision(2);
+        cout << "\t" << "Regular Hours worked: " << fixed << regularhoursworked << endl;
+        cout << "\t" << "Overtime Hours worked: " << fixed << overtimehours << endl;
+        cout << "\t" << "Gross Pay: " << endl;
+        cout << "\t\t" << "Regular Pay: " << fixed << regularpay << endl;
+        cout << "\t\t" << "Overtime Pay: " << fixed << overtimepay << endl;
+
+        cout << "\t" << "Deductions: " << endl;
+        cout << "\t\t" << "Federal Income Tax: " << fixed << federalincometax << endl;
+        cout << "\t\t" << "State Income Tax: " << fixed << stateincometax << endl;
+        cout << "\t\t" << "Social Security Tax: " << fixed << socialsecuritytax << endl;
+        if (additionalhealthcoveragecost > 0) 
+            cout << "\t\t" << "Additional health coverage cost: " << fixed << additionalhealthcoveragecost << endl;
+        cout << "\t" << "Net Pay: " << netpay << endl;
+        cout << "-----------------------------------------------------" << endl;
+
+        //  ask if user wants to run app again
+        do {
+            cout << endl << "Run the payroll program again [y/n]? ";
+            cin >> again;
+        } while (!(again == 'y' || again == 'Y' || again == 'n' || again == 'N'));
+
+        if (again == 'n' || again == 'N') endtask = true;
+    }while (!(endtask));
+}
